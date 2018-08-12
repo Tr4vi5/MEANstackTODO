d8a4f703f8e93ab0cc1e4240d9ac73fbeac23bc0
Tr4vi5 <trav.dunn@outlook.com>
2018-08-12 15:29:15 -0500

    routes made, connection to mongo issue

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,4 +3,38 @@ const myApp = angular.module('myApp', []);
 myApp.controller('TaskManager', function($http){
     console.log('Task Manager is ready');
     const vm = this;
+
+    vm.taskList = [];
+
+    vm.appendTasks = function() {
+        $http({
+            method: 'GET',
+            url: '/tasks'
+        }).then(function(response){
+            console.log('Got tasks:', response.data);
+            vm.taskList = response.data;
+        }).catch(function(error){
+            alert('Error in GET', error);
+        })
+    };// end appendTasks function
+
+    vm.sendTask = function(){
+        let taskToAdd = {
+            task: vm.taskIn,
+            category: vm.categoryIn,
+            description: vm.descriptionIn,
+            notes: vm.notesIn,
+            complete: vm.completeIn
+        }
+        $http({
+            method: 'POST',
+            url: '/tasks',
+            data: taskToAdd
+        }).then(function(response){
+            console.log('In sendTask POST', response.data);
+            vm.appendTasks();
+        }).catch(function(error){
+            alert('Error in sendTask POST', error);
+        });
+    };//end sendTask function
 })
--- server/router/task-router.js
+++ server/router/task-router.js
@@ -9,11 +9,33 @@ const TaskSchema = new Schema({
     category: {type: String},
     description: {type: String},
     notes: {type: String},
-    complete: {type: Boolean}
+    complete: {type: Boolean, default: false}
 });
 //model connecting new database collection to Schema
 const Tasks = mongoose.model('Tasks', TaskSchema);
 // routes
+router.get('/', (req, res)=>{
+    console.log('Successful GET from task-router');
+    Tasks.find({}).then((foundTasks)=>{
+        console.log('Found tasks from database:', foundTasks);
+        res.send(foundTasks);
+    }).catch((error)=>{
+        console.log('Error in GET request:', error);
+        res.sendStatus(500);
+    });
+});//end GET route
+router.post('/', (req, res)=>{
+    console.log('Successful POST to task-router', req.body);
+    let taskToAdd = req.body; // task from client to server 
+    let taskToSend = new Tasks(taskToAdd); // task from server to database
 
+    taskToSend.save().then(()=>{
+        console.log('Task saved to database', taskToSend);
+        res.sendStatus(200);
+    }).catch((error)=>{
+        console.log('Error in POST', error);
+        res.sendStatus(500);
+    });
+});//end POST route
 //export
 module.exports = router;
